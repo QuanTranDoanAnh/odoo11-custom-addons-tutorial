b8df4c8e85fe388267d9cb97184339f1beb4b7cf
QuanTranDoanAnh <td.anhquan@gmail.com>
2018-11-18 02:42:43 +0000

    After Contact Configuration

---
+++ openacademy/__manifest__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Open Academy",
+
+    'summary': """
+        Manage trainings""",
+
+    'description': """
+        Open Academy module for managing trainings:
+            - training courses
+            - training sessions
+            - attendees registration
+    """,
+
+    'author': "QuanTDA1-BLD",
+    'website': "http://www.bld.com.vn",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/11.0/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Tutorials',
+    'version': '0.1',
+    'application': True,
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'sale'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/openacademy.xml',
+        'views/partner.xml',
+        #'views/views.xml',
+        #'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
